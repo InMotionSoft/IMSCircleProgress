4dbad10005a10d4254214a02ac5929fa6ed0b682
Denis Romashov <denisromashow@gmail.com>
2015-10-07 16:29:28 +0300

    main swift file

---
+++ CircleProgress/Code/IMSCircleProgress.swift
@@ -0,0 +1,9 @@
+//
+//  IMSCircleProgress.swift
+//  CircleProgress
+//
+//  Created by Denis Romashov on 07.10.15.
+//  Copyright © 2015 InMotion Soft. All rights reserved.
+//
+
+import Foundation
